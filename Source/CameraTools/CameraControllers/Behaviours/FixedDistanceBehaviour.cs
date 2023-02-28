96d4dc88cb0a89a4ae8f8b974178bcbca78ed59c
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2023-02-28 22:23:50 +0100

    yes

--- Source/CameraTools/CameraControllers/Behaviours/FixedDistanceBehaviour.cs
+++ Source/CameraTools/CameraControllers/Behaviours/FixedDistanceBehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using CameraToolsKatnissified.UI;
+using CameraToolsKatnissified.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,29 +14,83 @@ namespace CameraToolsKatnissified.CameraControllers.Behaviours
         public enum Mode
         {
             /// This mode will keep the pivot exactly at the specified distance.
-            Fixed,
+            Constant,
             /// This mode will try to keep the pivot exactly at the specified distance. Maximum speed of travel is defined.
-            Ease
+            Velocity
         }
 
-
         public Mode _Mode { get; set; }
-
 
         /// The preferred distance from target.
         public float Distance { get; set; } = 100.0f;
         /// The minimum distance from target, which will never be exceeded. Can be null.
-        public float? MinDistance { get; set; }
+        public float? MinDistance { get; set; } = null;
         /// Maximum distance from target, which will never be exceeded. Can be null.
-        public float? MaxDistance { get; set; }
-
+        public float? MaxDistance { get; set; } = null;
 
         /// The behaviour will limit its movement speed to [0..Speed] per second (Speed * Time.fixedDeltaTime).
-        public float Speed { get; set; }
+        public float Speed { get; set; } = 20.0f;
 
         public FixedDistanceBehaviour( CameraPlayerController controller ) : base( controller )
         {
 
+        }
+
+        private float GetPosition1D( Vector3 dir, Vector3 position )
+        {
+            // returns position along the vector in one dimension.
+            // project the vector
+            // length = magnitude, sign = dot.
+            Vector3 newVector = Vector3.Project( position, dir );
+            if( Vector3.Dot( dir, newVector.normalized ) < 0 )
+            {
+                return -newVector.magnitude;
+            }
+            return newVector.magnitude;
+        }
+
+        private Vector3 GetPosition3D( Vector3 dir, float position )
+        {
+            // returns the position along the direction vector.
+            return dir * position;
+        }
+
+        private float Step( float position, float desiredPosition, float deltaTime )
+        {
+            // positive => closer to target than pivot.
+            // negative => closer to pivot than target.
+            // zero => at pivot.
+
+            // returns the new position.
+            if( _Mode == Mode.Constant )
+            {
+                return desiredPosition;
+            }
+
+            if( _Mode == Mode.Velocity )
+            {
+                float relativePosition = desiredPosition - position;
+                if( relativePosition > 0 ) // current position too high.
+                {
+                    float newPos = position - (Speed * deltaTime);
+                    if( newPos < desiredPosition ) // don't overshoot below the desired pos.
+                    {
+                        newPos = desiredPosition;
+                    }
+                    return newPos;
+                }
+                if( relativePosition < 0 ) // current position too low.
+                {
+                    float newPos = position + (Speed * deltaTime);
+                    if( newPos > desiredPosition ) // don't overshoot above the desired pos.
+                    {
+                        newPos = desiredPosition;
+                    }
+                    return newPos;
+                }
+                return desiredPosition; // Already at the target.
+            }
+            throw new InvalidOperationException( "Invalid mode" );
         }
 
         protected override void OnStartPlaying()
@@ -55,54 +110,70 @@ namespace CameraToolsKatnissified.CameraControllers.Behaviours
                     return;
                 }
 
-
+                Vector3 pivotPos = this.Pivot.position;
+                Vector3 targetPos = this.Controller.CameraTargetWorldSpace == null ? pivotPos : this.Controller.CameraTargetWorldSpace.Value;
+                Vector3 dir = this.Controller.CameraTargetWorldSpace == null ? this.Pivot.forward : (targetPos - pivotPos).normalized;
                 // project position onto vector from parent to target. (immediately snaps the camera inline).
 
+                float position = GetPosition1D( dir, pivotPos );
+                float desiredPosition = Distance;
 
-                Vector3 dir;
-                float desiredDisplacementFromParent;
-                float currentDisplacementFromParent = this.Pivot.localPosition.magnitude;
-                if( this.Controller.CameraTargetWorldSpace == null )
-                {
-                    desiredDisplacementFromParent = -Distance; // negative moves back.
-                    dir = this.Pivot.forward;
-                }
-                else
-                {
-                    Vector3 targetPos = this.Controller.CameraTargetWorldSpace.Value;
-                    float targetDist = Vector3.Distance( targetPos, this.Pivot.parent.position );
-                    dir = targetPos - this.Pivot.parent.position;
+                float newPosition = Step( position, desiredPosition, Time.fixedDeltaTime );
 
-                    desiredDisplacementFromParent = targetDist - Distance; // negative moves back.
-                }
-                // remaining = full - current as percentage of what is desired.
-                //float remainingDisplacementPercentage = 1 - (currentDisplacementFromParent / desiredDisplacementFromParent);
-#warning TODO - the camera goes away to infinity slowly when the parent lies too close.
+                // Never be further/closer from target than min/max distance.
+                newPosition = Mathf.Clamp( newPosition, MinDistance == null ? float.MinValue : MinDistance.Value, MaxDistance == null ? float.MaxValue : MaxDistance.Value );
 
-                float remainingDisplacement = desiredDisplacementFromParent - currentDisplacementFromParent;
-                //
+#warning TODO - doesn't work.
+                Vector3 newWorldPos = GetPosition3D( dir, newPosition );
 
-                Debug.Log( $"desiredDisplacementFromParent {desiredDisplacementFromParent}\n currentDisplacementFromParent {currentDisplacementFromParent}\n remainingDisplacement {remainingDisplacement}" );
-
-                dir.Normalize();
-
-                Vector3 displaceWorld = dir * desiredDisplacementFromParent;
-
-                this.Pivot.position = this.Pivot.parent.position + displaceWorld;
-
-
-
-                // clamp in range of min to max distance.
+                this.Pivot.position = newWorldPos;
             }
         }
 
-
         public override void DrawGui( UILayout UILayout, ref int line )
         {
+            GUI.Label( UILayout.GetRectX( line, 1, 9 ), $"_Mode: {_Mode}" );
+            if( GUI.Button( UILayout.GetRect( 10, line ), "<" ) )
+            {
+                _Mode = Misc.CycleEnum( _Mode, -1 );
+            }
+            if( GUI.Button( UILayout.GetRect( 11, line ), ">" ) )
+            {
+                _Mode = Misc.CycleEnum( _Mode, 1 );
+            }
+            line++;
+            // button cycleenum. Mode
+
+            GUI.Label( UILayout.GetRectX( line, 1, 6 ), "Distance:" );
+            Distance = float.Parse( GUI.TextField( UILayout.GetRectX( line, 7, 11 ), Distance.ToString() ) );
+            line++;
+
+            GUI.Label( UILayout.GetRectX( line, 1, 6 ), "MinDistance:" );
+            if( float.TryParse( GUI.TextField( UILayout.GetRectX( line, 7, 11 ), MinDistance.ToString() ), out float minDist ) )
+            {
+                MinDistance = minDist;
+            }
+            else
+            {
+                MinDistance = null;
+            }
+            line++;
+
+            GUI.Label( UILayout.GetRectX( line, 1, 6 ), "MaxDistance:" );
+            if( float.TryParse( GUI.TextField( UILayout.GetRectX( line, 7, 11 ), MaxDistance.ToString() ), out float maxDist ) )
+            {
+                MaxDistance = maxDist;
+            }
+            else
+            {
+                MinDistance = null;
+            }
+            line++;
+
+            GUI.Label( UILayout.GetRectX( line, 1, 6 ), "Speed:" );
+            Speed = float.Parse( GUI.TextField( UILayout.GetRectX( line, 7, 11 ), Speed.ToString() ) );
+            line++;
         }
-
-
-
 
         // This behaviour is supposed to keep its transform at a constant distance from the camera target.
 
@@ -110,5 +181,4 @@ namespace CameraToolsKatnissified.CameraControllers.Behaviours
         // The axis of offset is determined by the parent's position and the target position.
         // If parent is null, it offsets along the forward axis.
     }
-
 }
--- Source/CameraTools/CameraControllers/Behaviours/FreeBehaviour.cs
+++ Source/CameraTools/CameraControllers/Behaviours/FreeBehaviour.cs
@@ -13,12 +13,10 @@ namespace CameraToolsKatnissified.CameraControllers.Behaviours
         private float[] _values; // circular array, inputs are replaced oldest to latest wraparound.
         private int _oldest = 0;
 
-
         public InputBuffer( int length )
         {
             _values = new float[length]; // by default, inputs are 0's.
         }
-
 
         public void Add( float input )
         {
@@ -26,7 +24,6 @@ namespace CameraToolsKatnissified.CameraControllers.Behaviours
             _oldest++;
             _oldest %= _values.Length;
         }
-
 
         public float GetAverage()
         {
@@ -43,42 +40,35 @@ namespace CameraToolsKatnissified.CameraControllers.Behaviours
     {
         /// How many frames to smooth over. More = smoother input, but less responsive.
         /// >1 - smoothing, ==1 - no smoothing, <=0 - invalid.
-        public int SmoothLength { get; set; }
-
+        public int SmoothLength { get; set; } = 10;
 
         /// Multiplier for scroll wheel input.
-        public float ScrollSensitivity { get; set; }
+        public float ScrollSensitivity { get; set; } = 1.0f;
         /// Multiplier for mouse input.
-        public float MouseSensitivity { get; set; }
-
+        public float MouseSensitivity { get; set; } = 1.0f;
 
         /// Multiplier for keyboard input.
-        public float KeyboardSensitivity { get; set; }
+        public float KeyboardSensitivity { get; set; } = 1.0f;
 
-
-
-
-        public float MaxAcceleration { get; set; }
+        public float MaxAngularAcceleration { get; set; } = 1.0f;
+        public float MaxAcceleration { get; set; } = 1.0f;
         /// Every frame, the _velocity and _angularVelocity are multiplied with this.
-        public float Drag { get; set; }
-        public float AngularDrag { get; set; }
-
+        public float Drag { get; set; } = 0.9f;
+        public float AngularDrag { get; set; } = 0.9f;
 
         bool _isOrbiting;
-
 
         Vector3 _velocityWS;
         Quaternion _angularVelocityWS;
         float _scrollVelocity;
 
+        Vector3 _upDir;
 
         InputBuffer _mouseXBuffer;
         InputBuffer _mouseYBuffer;
         InputBuffer _scrollWheelBuffer;
-        InputBuffer _LeftArrowBuffer;
-        InputBuffer _RightArrowBuffer;
-        InputBuffer _UpArrowBuffer;
-        InputBuffer _DownArrowBuffer;
+        InputBuffer _moveForwardBuffer;
+        InputBuffer _moveSidewaysBuffer;
 
         public FreeBehaviour( CameraPlayerController controller ) : base( controller )
         {
@@ -87,40 +77,56 @@ namespace CameraToolsKatnissified.CameraControllers.Behaviours
 
         // Behaviour controlled by user input.
 
-
         // Smoothing uses an array storing the user inputs from SmoothLength last frames.
         // Those inputs are then averaged to produce the final input value.
-
 
         // Supported Movement Types:
         // - Panning (mouse)
         // - Orbiting (around the cpc.CameraTargetWorldSpace)
         // - Translation (mouse)
 
-
         // - Translation (keyboard)
         // - Rotation (keyboard)
 
+        protected override void OnStartPlaying()
+        {
+            if( FlightCamera.fetch.mode == FlightCamera.Modes.ORBITAL || (FlightCamera.fetch.mode == FlightCamera.Modes.AUTO && FlightCamera.GetAutoModeForVessel( Ctm.ActiveVessel ) == FlightCamera.Modes.ORBITAL) )
+            {
+                _upDir = Vector3.up;
+            }
+            else
+            {
+                _upDir = -FlightGlobals.getGeeForceAtPosition( Ctm.ActiveVessel.GetWorldPos3D() ).normalized;
+            }
+            ApplySmoothLength();
+            _velocityWS = Vector3.zero;
+            _angularVelocityWS = Quaternion.identity;
+        }
 
         void ApplySmoothLength()
         {
             _mouseXBuffer = new InputBuffer( this.SmoothLength );
             _mouseYBuffer = new InputBuffer( this.SmoothLength );
             _scrollWheelBuffer = new InputBuffer( this.SmoothLength );
-            _LeftArrowBuffer = new InputBuffer( this.SmoothLength );
-            _RightArrowBuffer = new InputBuffer( this.SmoothLength );
-            _UpArrowBuffer = new InputBuffer( this.SmoothLength );
-            _DownArrowBuffer = new InputBuffer( this.SmoothLength );
+            _moveForwardBuffer = new InputBuffer( this.SmoothLength );
+            _moveSidewaysBuffer = new InputBuffer( this.SmoothLength );
         }
 
-
-        public override void Update( bool onPlaying )
+        public override void Update( bool isPlaying )
         {
-            _mouseXBuffer.Add( Input.GetAxis( "Mouse Horizontal" ) );
-            _mouseYBuffer.Add( Input.GetAxis( "Mouse Vertical" ) );
-            // ...
+            if( isPlaying )
+            {
+                _mouseXBuffer.Add( Input.GetAxis( "Mouse X" ) );
+                _mouseYBuffer.Add( Input.GetAxis( "Mouse Y" ) );
+                _scrollWheelBuffer.Add( Input.GetAxis( "Mouse ScrollWheel" ) );
+                float right = Input.GetKey( KeyCode.LeftArrow ) ? -1.0f : 0.0f;
+                right += Input.GetKey( KeyCode.RightArrow ) ? 1.0f : 0.0f;
+                float fwd = Input.GetKey( KeyCode.UpArrow ) ? 1.0f : 0.0f;
+                fwd += Input.GetKey( KeyCode.DownArrow ) ? -1.0f : 0.0f;
+                _moveForwardBuffer.Add( fwd );
+                _moveSidewaysBuffer.Add( right );
+            }
         }
-
 
         Vector3 GetVelocity( Quaternion angularVelocity, Vector3 where )
         {
@@ -129,7 +135,6 @@ namespace CameraToolsKatnissified.CameraControllers.Behaviours
             // tangential velocity magnitude per unit time is radius * angular displacement per unit time
             return Vector3.Cross( axis, where.normalized ) * where.magnitude * anglePerSec; // might need to be converted to radians.
         }
-
 
         /// Returns the angular velocity for a circular orbit at a distance from the origin and its tangent velocity.
         Quaternion GetAngularVelocity( Vector3 velocity, Vector3 where )
@@ -144,115 +149,124 @@ namespace CameraToolsKatnissified.CameraControllers.Behaviours
             // theta = v / r
             float anglePerSec = velocity.magnitude / where.magnitude; // angle = velocity / radius
             return Quaternion.AngleAxis( anglePerSec, axis ); // angle possibly might need to be converted to degrees.
-                                                                  // vice versa on the other end.
+                                                              // vice versa on the other end.
         }
 
-
-        public override void FixedUpdate( bool onPlaying )
+        public override void FixedUpdate( bool isPlaying )
         {
-            float mouseX = _mouseXBuffer.GetAverage();
-            float mouseY = _mouseYBuffer.GetAverage();
-            // ...
-
-
-            // If the player starts orbiting and lets go,
-            // then the camera continues moving tangentially with the velocity it had before.
-            if( Input.GetKey( KeyCode.Mouse1 ) )
+            if( isPlaying )
             {
-                if( Input.GetKey( KeyCode.Mouse2 ) )
+                float mouseX = _mouseXBuffer.GetAverage();
+                float mouseY = _mouseYBuffer.GetAverage();
+                float mouseScroll = _scrollWheelBuffer.GetAverage();
+                float forward = _moveForwardBuffer.GetAverage();
+                float sideways = _moveSidewaysBuffer.GetAverage();
+                // ...
+                // If the player starts orbiting and lets go,
+                // then the camera continues moving tangentially with the velocity it had before.
+                if( Input.GetKey( KeyCode.Mouse1 ) ) // mouse1 - right
                 {
-                    // pan.
-                }
-                else
-                {
-                    if( this.Controller.CameraTargetWorldSpace != null )
+                    if( Input.GetKey( KeyCode.Mouse0 ) ) // mouse2 - middle
                     {
+                        // x inputaxis, y inputaxis copy from other controller, but add to velocity instead.
+                        // limit to max acceleration.
+                        // how do we limit? degrees per second.
+
+                        Debug.Log( "PAN" );
+                        this._angularVelocityWS *= Quaternion.AngleAxis( mouseX * MaxAngularAcceleration * Time.fixedDeltaTime, Vector3.up );
+                        this._angularVelocityWS *= Quaternion.AngleAxis( -mouseY * MaxAngularAcceleration * Time.fixedDeltaTime, Vector3.right );
+                        //_angularVelocityWS.ToAngleAxis( out float a, out Vector3 ax );
+                        //this._angularVelocityWS = Quaternion.LookRotation( ax, _upDir );
+
+                        // pan.
+                    }
+                    else
+                    {
+                        Debug.Log( "ORBIT" );
                         if( !_isOrbiting )
                         {
+                            Debug.Log( "ORBIT 2" );
                             // get angular velocity from velocity and distance.
                             this._angularVelocityWS = GetAngularVelocity( this._velocityWS, this.Pivot.position - this.Controller.CameraTargetWorldSpace.Value );
                             this._velocityWS = Vector3.zero;
                         }
+                        else
+                        {
+                            _isOrbiting = true;
+                        }
                         // orbit.
 
-                        _isOrbiting = true;
+                        this._angularVelocityWS *= Quaternion.AngleAxis( mouseX * MaxAngularAcceleration * Time.fixedDeltaTime, Vector3.up );
+                        this._angularVelocityWS *= Quaternion.AngleAxis( -mouseY * MaxAngularAcceleration * Time.fixedDeltaTime, Vector3.right );
+                        //_angularVelocityWS.ToAngleAxis( out float a, out Vector3 ax );
+                        //this._angularVelocityWS = Quaternion.LookRotation( ax, _upDir );
+
                     }
                 }
-            }
-            else
-            {
-                if( _isOrbiting )
+                else
                 {
-                    if( this.Controller.CameraTargetWorldSpace != null )
+                    if( _isOrbiting )
                     {
+                        Debug.Log( "o3" );
                         // convert orbital angular velocity to normal velocity.
                         // rotational (angular) velocity remains so that it's not abruptly stopping.
                         this._velocityWS = GetVelocity( this._angularVelocityWS, this.Pivot.position - this.Controller.CameraTargetWorldSpace.Value );
+
+                        _isOrbiting = false;
                     }
-
-                    _isOrbiting = false;
                 }
 
-
-                if( Input.GetKey( KeyCode.Mouse2 ) )
+                if( Input.GetKey( KeyCode.LeftArrow ) || Input.GetKey( KeyCode.RightArrow )
+                 || Input.GetKey( KeyCode.UpArrow ) || Input.GetKey( KeyCode.DownArrow ) )
                 {
+                    const float MOVE_SCALE = 1.0f;
                     // xy movement.
+                    Vector3 forwardAcceleration = this.Pivot.forward * forward;
+                    Vector3 rightAcceleration = this.Pivot.right * sideways;
 
+                    Vector3 sum = forwardAcceleration + rightAcceleration;
 
+                    sum *= MaxAcceleration * MOVE_SCALE;
 
+                    this._velocityWS += sum;
+                }
+#warning TODO - camera values spin out of control.
+
+                _angularVelocityWS.ToAngleAxis( out float angle, out Vector3 axis );
+
+                float scaledAngle = angle * Time.fixedDeltaTime;
+                Quaternion scaledAngularVelocity = Quaternion.AngleAxis( scaledAngle, axis );
+
+                // Apply the accelerations based on avg inputs.
+                if( _isOrbiting )
+                {
 
                 }
+                else
+                {
+                    this.Pivot.position += this._velocityWS * Time.fixedDeltaTime;
+                    this.Pivot.rotation *= scaledAngularVelocity;
+                }
+
+                /*left + right
+                left + middle
+                middle + right
+                left
+                right
+                middle*/
+
+                // middle - xy movement camera.forward + camera.right
+                // right - orbit
+                // middle + right - pan
+
+                // scroll wheel zooms you in and out.
+                // - as a multiplier to the normal Zoom value.
+
+                _velocityWS *= Drag;
+
+                angle *= AngularDrag;
+                _angularVelocityWS = Quaternion.AngleAxis( angle, axis );
             }
-
-
-            _angularVelocityWS.ToAngleAxis( out float angle, out Vector3 axis );
-
-
-            float scaledAngle = angle * Time.fixedDeltaTime;
-            Quaternion scaledAngularVelocity = Quaternion.AngleAxis( scaledAngle, axis );
-
-
-            // Apply the accelerations based on avg inputs.
-            if( _isOrbiting )
-            {
-
-
-            }
-            else
-            {
-                this.Pivot.position += this._velocityWS * Time.fixedDeltaTime;
-                this.Pivot.rotation *= scaledAngularVelocity;
-            }
-
-
-            /*left + right
-            left + middle
-            middle + right
-            left
-            right
-            middle*/
-
-
-            // middle - xy movement camera.forward + camera.right
-            // right - orbit
-            // middle + right - pan
-
-
-            // scroll wheel zooms you in and out.
-            // - as a multiplier to the normal Zoom value.
-
-
-
-
-            _velocityWS *= Drag;
-
-
-            angle *= AngularDrag;
-            _angularVelocityWS = Quaternion.AngleAxis( angle, axis );
-        }
-
-        protected override void OnStartPlaying()
-        {
         }
 
         protected override void OnStopPlaying()
@@ -261,7 +275,50 @@ namespace CameraToolsKatnissified.CameraControllers.Behaviours
 
         public override void DrawGui( UILayout UILayout, ref int line )
         {
+            GUI.Label( UILayout.GetRectX( line, 0, 6 ), "Smooth length:" );
+            int oldSmooth = SmoothLength;
+            SmoothLength = int.Parse( GUI.TextField( UILayout.GetRectX( line, 7, 11 ), SmoothLength.ToString() ) );
+            if( SmoothLength <= 0 )
+            {
+                SmoothLength = oldSmooth;
+            }
+            if( oldSmooth != SmoothLength )
+            {
+                ApplySmoothLength();
+            }
+            line++;
+
+            GUI.Label( UILayout.GetRectX( line, 0, 6 ), "Scroll sens.:" );
+            ScrollSensitivity = GUI.HorizontalSlider( UILayout.GetRectX( line, 7, 11 ), ScrollSensitivity, 0.01f, 10.0f );
+            line++;
+
+            GUI.Label( UILayout.GetRectX( line, 0, 6 ), "Scroll sens.:" );
+            ScrollSensitivity = GUI.HorizontalSlider( UILayout.GetRectX( line, 7, 11 ), ScrollSensitivity, 0.01f, 10.0f );
+            line++;
+
+            GUI.Label( UILayout.GetRectX( line, 0, 6 ), "Mouse sens.:" );
+            MouseSensitivity = GUI.HorizontalSlider( UILayout.GetRectX( line, 7, 11 ), MouseSensitivity, 0.01f, 10.0f );
+            line++;
+
+            GUI.Label( UILayout.GetRectX( line, 0, 6 ), "Keyboard sens.:" );
+            KeyboardSensitivity = GUI.HorizontalSlider( UILayout.GetRectX( line, 7, 11 ), KeyboardSensitivity, 0.01f, 10.0f );
+            line++;
+
+            GUI.Label( UILayout.GetRectX( line, 0, 6 ), "Max Acceleration:" );
+            MaxAcceleration = float.Parse( GUI.TextField( UILayout.GetRectX( line, 7, 11 ), MaxAcceleration.ToString() ) );
+            line++;
+
+            GUI.Label( UILayout.GetRectX( line, 0, 6 ), "Max Angular Acceleration:" );
+            MaxAngularAcceleration = float.Parse( GUI.TextField( UILayout.GetRectX( line, 7, 11 ), MaxAngularAcceleration.ToString() ) );
+            line++;
+
+            GUI.Label( UILayout.GetRectX( line, 0, 6 ), "Drag:" );
+            Drag = float.Parse( GUI.TextField( UILayout.GetRectX( line, 7, 11 ), Drag.ToString() ) );
+            line++;
+
+            GUI.Label( UILayout.GetRectX( line, 0, 6 ), "Angular Drag:" );
+            AngularDrag = float.Parse( GUI.TextField( UILayout.GetRectX( line, 7, 11 ), AngularDrag.ToString() ) );
+            line++;
         }
     }
-
 }
